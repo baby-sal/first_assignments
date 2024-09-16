f1a7209f8756f00b6dc1f671b0e40e53b6e20d49
baby-sal <sally.facebook123@gmail.com>
2024-09-16 20:13:14 +0100

    Cleaning up scratch .py files, film search, adding improvement points.

--- assignment_2_python/full_code.py
+++ assignment_2_python/full_code.py
@@ -1,12 +1,7 @@
 # **Studio Ghibli Character Generator and Movie Recommendation**
-# Which studio Ghibli character do you look like?
-# Which studio Ghibli movie do they appear in?
-#
-# Create a welcome message to explain what the purpose of the program is
-# Ask the user to input their gender, hair colour and eye colour
-# Send the inputs to the API to return the characters that you have similar features
-# Give the option of which character the user would like to learn about.
-# Display the characters details and what films they appear in (title, original title, release year, description, rotten tomato score(put highest ranking on top?))
+
+
+
 import requests
 import json
 import time
@@ -30,11 +25,11 @@ user["name"] = input("Hello friend, how do I spell your name?")
 read_out_my_name(user["name"])
 
 # Explain the program to the user:
-print(f'''\n{user["name"]}, it's nice to meet you! \nIf you can kindly share some details with me, I'd like to find your Ghibli doppelganger and recommend you a movie. \nIf that sounds ok, let's get spirited away!''')
+print(f"{user["name"]}, it's nice to meet you! \n\nIf you can kindly share some details with me, I'd like to find your Ghibli doppelganger and recommend you a movie. \n\nIf that sounds ok, let's get spirited away!")
 
 # Ask for the user's inputs
-# Name, creating a themed nickname
-user_gender = input("What is your gender?")
+# Creating a themed nickname:
+user_gender = input("\nWhat is your gender?")
 
 def nickname(suffix):
     if user_gender == "Male" or user_gender == "male":
@@ -46,73 +41,85 @@ def nickname(suffix):
 
     full_name = (user["name"] + suffix)
 
-    print(f"OK! We shall call you " + full_name+"!")
+    print(f"\nOK! We shall call you " + full_name+"!")
     return full_name
-# Collecting hair colour and eye colour
 
+# Collecting hair colour and eye colour
 user_name = nickname("")
 
-ghibli_hair = input("What is your hair colour?  ").capitalize()
+ghibli_hair = input("\nWhat is your hair colour? ").capitalize()
 
-endpoint = f"https://ghibliapi.vercel.app/people?hair_color={ghibli_hair}"
-response = requests.get(endpoint)
-data = response.json()
+endpoint_hair = f"https://ghibliapi.vercel.app/people?hair_color={ghibli_hair}"
+response_hair = requests.get(endpoint_hair)
+data_hair = response_hair.json()
 
-# print the names
-ghibli_name = [person["name"] for person in data]
-if ghibli_name:
-    print(", ".join(ghibli_name) + " has the same hair colour as you!")
+# print the names of matching hair colour characters
+hair_name = [person["name"] for person in data_hair]
+if hair_name:
+    print(", ".join(hair_name) + " has the same hair colour as you!")
 else:
-    print("Sorry no match, try again?")
+    print("\nSorry no match, try again?")
 
-print(f"I'm finding so much out about you! One more thing...")
+print(f"\nI'm finding so much out about you! One more thing...")
 
-ghibli_eyes = input("I can't help getting lost in your eyes, " + user_name +", but what is their colour?").capitalize()
+ghibli_eyes = input("\nI can't help getting lost in your eyes, " + user_name +", but what is their colour?").capitalize()
 
-endpoint_2 = f"https://ghibliapi.vercel.app/people"
-response_2 = requests.get(endpoint_2)
-data_2 = response_2.json()
+endpoint_eyes = f"https://ghibliapi.vercel.app/people"
+response_eyes = requests.get(endpoint_eyes)
+data_eyes = response_eyes.json()
 
-# print the names
-ghibli_name_2 = [person["name"] for person in data_2 if person["eye_color"].capitalize() == ghibli_eyes]
-if ghibli_name_2:
-    print(", ".join(ghibli_name_2) + " has the same eye colour as you!")
+# print the names of matching eye colour characters
+eyes_name = [person["name"] for person in data_eyes if person["eye_color"].capitalize() == ghibli_eyes]
+if eyes_name:
+    print(", ".join(eyes_name) + " has the same eye colour as you!")
 else:
-    print("Sorry no match, try again?")
+    print("\nSorry no match, try again?")
 
-print("Now let's see if you have a doppelganger or not...")
+print("\nNow let's see if you have a doppelganger or not...")
+
+# I used the time module here to make the user have some wait time which increases their suspense.
 time.sleep(3.0)
 
-# Testing the doppelganger finder
-doppelganger_name = [name for name in ghibli_name if name in ghibli_name_2]
+# Doppelganger finder
+doppelganger_name = [name for name in hair_name if name in eyes_name]
 
 if doppelganger_name:
-    print("Your Ghibli doppelganger is: " + ", ".join(doppelganger_name))
+    print("\nYour Ghibli doppelganger is: " + ", ".join(doppelganger_name))
     file = open("/Users/sallydavies/Desktop/CFG Degree/CFG-Assignments/assignment_2_python/doppelgangers", "a+")
     file.write(f"\n {user_name} looks like {doppelganger_name}")
     file.close()
 else:
-    print("Sorry no match...")
+    print("\nSorry no match...")
 
-# Connecting doppelganger with films
+# Connecting doppelganger with films and producing a recommendation
 
-interest_cha = input("Which character would you search in movies?").capitalize()
+interest_cha = input("\nWhich character would you search in movies?").capitalize()
 
-interest_films = []
-for person in data:
-    if person["name"].capitalize() == interest_cha:
-        interest_films = person["films"]
+endpoint_films = f"https://ghibliapi.vercel.app/films"
+response_films = requests.get(endpoint_films)
+data_films = response_films.json()
 
-if interest_films:
-    print (f"{interest_cha} is in the following films:")
-    for film_url in interest_films:
-        film_response = requests.get(film_url)
-        film_data = film_response.json()
-        print(f"{film_data["title"]} which was produced by {film_data["director"]}")
-        release_year = str(film_data["release_date"])
-        print(f"It was released in '" + release_year[2:4])
-        print(f"Here is a quick description: \n {film_data["description"]}")
-        print(f"This film garnered a Rotten Tomatoes score of {film_data["rt_score"]} /100, why don't you check it out?!")
-        file = open("/Users/sallydavies/Desktop/CFG Degree/CFG-Assignments/assignment_2_python/results.txt", "a+")
-        file.write(f"\n {film_data["title"]}")
-        file.close()
+url_name = [person["films"] for person in data_eyes if person["name"].capitalize() == interest_cha]
+
+if url_name:
+    print(interest_cha + f" is in the following film:\n")
+for film in data_films:
+        if film["url"] in url_name[0]:
+            print(f"{film["title"]}, \n Which was produced by the world-famous {film["director"]}.")
+            release_year = str(film["release_date"])
+            print(f"It was released in '" + release_year[2:4])
+            print(f"Here is a quick description: \n {film["description"]}")
+            print(f"This film garnered a Rotten Tomatoes score of {film["rt_score"]} /100, why don't you check it out?!")
+            file = open("/Users/sallydavies/Desktop/CFG Degree/CFG-Assignments/assignment_2_python/results.txt", "a+")
+            file.write(f"\n {film["title"]}")
+            file.close()
+else:
+    print("\nSorry no match, try again?")
+
+print("I hope you enjoyed the Ghibli quiz! ありがとうございました")
+print("	♡＼(￣▽￣)／♡")
+
+
+# How I'd like to improve:
+# Recursion - if there is no match then being able to repeat that input again
+# Be able to connect related colours of hair and eyes, so that there are more matches (some of the colour names are too specific)
--- assignment_2_python/string_split.py
+++
@@ -1,4 +0,0 @@
-# Convert the year into a string
-release_year = str(1986)
-# Split the string and print
-print("'" + release_year[2:4])
