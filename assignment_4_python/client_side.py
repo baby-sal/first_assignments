a2de44fb838706fe183c88e0f8fb48b438c67548
baby-sal <sally.facebook123@gmail.com>
2024-10-06 17:44:32 +0100

    Working a little on client side and writing a checklist for the assignment.

--- assignment_4_python/client_side.py
+++ assignment_4_python/client_side.py
@@ -4,7 +4,14 @@ app = Flask(__name__)
 
 import requests
 
-def get_food():
+@app.route("/food/<int:food_id>", methods=["GET"])
+def get_food(food_id):
     endpoint = "http://127.0.0.1:5000/food/"
     data = requests.get(endpoint).json()
+    return data
+
+@app.route("/review/add", methods=["POST"])
+def add_review():
+    endpoint_2 = "http://127.0.0.1:5000/review/"
+    data = requests.get(endpoint_2).json()
     return data
