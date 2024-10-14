c7eb1e2e8f98b29062ee5cdb79894f0b26ccae06
baby-sal <sally.facebook123@gmail.com>
2024-10-14 21:18:13 +0100

    Cleaning up app by removing commented out workings.

--- assignment_4_python/app.py
+++ assignment_4_python/app.py
@@ -1,16 +1,11 @@
-# Implement 2 API endpoints with appropriate functionality Implement one additional endpoint of your choice can be POST or GET (APP)
-
-
 from flask import Flask, jsonify, request
 from requests import delete
-
-from db_utils import get_all_food, get_all_reviews, delete_review_by_id, add_review
+from db_utils import get_all_food, get_all_reviews, delete_review_by_id_db
 
 app = Flask(__name__)
 
-
-@app.route("/food/<int:food_id>", methods=["GET"])
-def get_food(food_id):
+@app.route("/food", methods=["GET"])
+def get_food():
     return jsonify(get_all_food())
 
 
@@ -21,22 +16,7 @@ def get_reviews():
 
 @app.route("/reviews/delete/<int:id>", methods=["DELETE"])
 def delete_review_by_id(id):
-    return jsonify(delete_review_by_id(id))
-
-
-@app.route("/reviews/add", methods=["PUT"])
-def add_review():
-    review = request.get_json()
-    add_review(
-        food_id=review["food_id"],
-        review_date=review["review_date"],
-        review=review["review"],
-    )
-    return review
-
-
-
-###This was from other file you created called 'app'
+    return jsonify(delete_review_by_id_db(id))
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
